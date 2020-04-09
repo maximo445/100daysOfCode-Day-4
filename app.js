637c32987332957cb3bddf903487cd7417a26ad1
maximo445 <britofilmmaker@gmail.com>
2020-04-09 12:28:38 -0400

    Phase one completed!

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-//Adding people constants
   const addPassengerInput = document.querySelector('.addPassengerInput');
   const addPassengerButton = document.querySelector('.addPassengerButton');
   const addList = document.querySelector('.atCourtList');
-  const addBackButton = document.querySelector('.addBackButton');
   const historyList = document.querySelector('.historyList');
   const content = document.querySelector('.content');
-  const deleteButton = document.querySelector('.deleteButton');
   const notification = document.querySelector('.notification');
-  const removeButton = document.querySelector('.removeButton');
   const tripHistoryBottom = document.querySelector('.tripHistoryBottom');
-
-  let removeCheckBoxes;
-  let atCourtPeople;
-  let itemToHistory;
-  let historyCheckBoxes;
-  let atHistoryPeople;
 
   content.addEventListener('click', (event) => {
     if (event.target.className == 'addPassengerButton') {
@@ -22,15 +12,15 @@
       let addLiElement = document.createElement('li');
       addLiElement.className = "atCourtPeople";
 
-      let addInputElement = document.createElement('input')
-      addInputElement.className = "removeCheckBoxes";
-      addInputElement.type = "checkbox";
+      let addInputElement = document.createElement('button')
+      addInputElement.className = "removeButton";
+      addInputElement.textContent = "Remove";
 
       if (addPassengerInput.value === "") {
         addPassengerInput.value = "Unknown";
       }
 
-      addLiElement.textContent = addPassengerInput.value
+      addLiElement.textContent = addPassengerInput.value;
       addLiElement.appendChild(addInputElement);
 
       addList.appendChild(addLiElement);
@@ -45,81 +35,92 @@
       }
     }
 
-    if (event.target.className == 'removeButton') {
-      loop('.atCourtPeople', '.removeCheckBoxes', 'removeButton')
-    }
-
-    if (event.target.className == 'addBackButton') {
-      loop('.atHistoryPeople','.historyCheckBoxes','addBackButton');
-    }
-
-    if (event.target.className == 'deleteButton') {
-      loop('.atHistoryPeople', '.historyCheckBoxes', 'deleteButton');
-    }
+    transporter(event.target);
 
   });
 
-  function loop (peopleClass, checkboxesClass, myEvent) {
-    let people = document.querySelectorAll(peopleClass);
-    let checkboxes = document.querySelectorAll(checkboxesClass);
+  function transporter (myEvent) {
 
-    let person = [];
-
-    if (myEvent == 'removeButton') {
-      for (let i = 0; i < people.length; i++) {
-        if(checkboxes[i].checked === true){
-          let itemToHistory = document.createElement('li');
-          itemToHistory.className = "atHistoryPeople";
-          itemToHistory.innerHTML = people[i].textContent + "   " + '<input  class="historyCheckBoxes" type="checkbox" name="" value="">';
-          historyList.appendChild(itemToHistory);
-
-          people[i].parentNode.removeChild(people[i]);
-          checkboxes[i].remove;
-        }
-      }
+    if (myEvent.className == 'removeButton') {
+      let li = refactorLiElement('Add Back', 'addBackButton');
+      let myDeleteButton = document.createElement('button');
+      myDeleteButton.textContent = 'Delete';
+      myDeleteButton.className = 'deleteButton';
+      li.appendChild(myDeleteButton);
+      historyList.appendChild(li);
     }
 
-    if (myEvent == 'addBackButton') {
-      for (let i = 0; i < people.length; i++) {
-        if(checkboxes[i].checked === true){
-
-          let addLiElement = document.createElement('li');
-          addLiElement.className = "atCourtPeople";
-          addLiElement.innerHTML = people[i].textContent + '<input  class="removeCheckBoxes" type="checkbox" name="" value="">';
-
-          addList.appendChild(addLiElement);
-
-          people[i].parentNode.removeChild(people[i]);
-          checkboxes[i].remove;
-        }
-      }
+    if (myEvent.className == 'addBackButton') {
+      let liToCourt = refactorLiElement('Remove', 'removeButton');
+      addList.appendChild(liToCourt);
     }
 
-    if (myEvent == 'deleteButton') {
-      for (let i = 0; i < people.length; i++) {
-        if(checkboxes[i].checked === true){
-          people[i].parentNode.removeChild(people[i]);
-          checkboxes[i].remove;
-          person.push(i);
-        }
-      }
-      whoGotChanged(people[person]);
-      person = [];
+    if (myEvent.className == 'deleteButton') {
+      let liToDelete = myEvent.parentNode;
+      let parentOl = liToDelete.parentNode;
+
+      whoGotChanged(getText(liToDelete));
+
+      parentOl.removeChild(liToDelete);
     }
+  }
+
+  function refactorLiElement (name, myClass) {
+    let myButton = event.target;
+
+    let temporalButton = document.createElement('button');
+    temporalButton.className = myClass;
+    temporalButton.textContent = name;
+
+    let myLi = myButton.parentNode;
+    let myOl = myLi.parentNode;
+
+    let temporalLi = document.createElement('li');
+
+    // let textArray = [].reduce.call(myLi.childNodes, (a, b) => {
+    //
+    //   return a + (b.nodeType === 3 ? b.textContent : '');
+    //
+    // } ,'');
+    //
+    // let text = "";
+    //
+    // for (let i = 0; i < textArray.length; i++) {
+    //   text += textArray[i];
+    // }
+
+    temporalLi.textContent = getText(myLi);
+
+    temporalLi.appendChild(temporalButton);
+
+    myOl.removeChild(myLi);
+
+    return temporalLi;
   }
 
   function whoGotChanged (myPerson) {
 
     notification.style.color = 'red';
-
-    if (myPerson.length > 1) {
-      notification.textContent = myPerson.length.toString() + ' deleted';
-    } else {
-      notification.textContent = myPerson.textContent.substr(0,7);
-    }
+    notification.textContent = myPerson.substr(0,7);
 
     window.setTimeout((myString) => {
       notification.textContent = myString;
     }, 1000, "");
 
+  }
+
+  function getText(parent) {
+    let textArray = [].reduce.call(parent.childNodes, (a, b) => {
+
+      return a + (b.nodeType === 3 ? b.textContent : '');
+
+    } ,'');
+
+    let text = "";
+
+    for (let i = 0; i < textArray.length; i++) {
+      text += textArray[i];
+    }
+
+    return text;
   }
